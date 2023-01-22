67217c322039730c66dbca73bcc892f54e7e554c
Tiago Becerra Paolini <tpaolini@gmail.com>
2023-01-22 17:03:47 -0300

    Password hashing

--- src/imc_crypto.c
+++ src/imc_crypto.c
@@ -1,1 +1,38 @@
+#include <stdint.h>
+#include <string.h>
+#include <sodium.h>
 #include "imc_crypto.h"
+
+static const char IMC_SALT[crypto_pwhash_SALTBYTES+1] = "imageconceal2023";
+
+// Generate a secret key from a password
+int imc_crypto_context_create(char *password, CryptoContext **out)
+{
+    CryptoContext *context = sodium_malloc(sizeof(CryptoContext));
+    if (!context) return IMC_ERR_NO_MEMORY;
+    
+    uint8_t output[sizeof(CryptoContext)];
+    sodium_mlock(output, sizeof(output));
+    
+    int status = crypto_pwhash(
+        (uint8_t * const)&output,
+        sizeof(output),
+        password,
+        strlen(password),
+        IMC_SALT,
+        IMC_OPSLIMIT,
+        IMC_MEMLIMIT,
+        crypto_pwhash_ALG_ARGON2ID13
+    );
+    if (status < 0) return IMC_ERR_INVALID_PASS;
+
+    *context = (CryptoContext){
+        .xcc20_key = *(uint64_t*)(&output[0]),
+        .bbs_seed  = *(uint64_t*)(&output[sizeof(uint64_t)])
+    };
+    
+    sodium_munlock(output, sizeof(output));
+
+    *out = context;
+    return 0;
+}
--- src/imc_crypto.h
+++ src/imc_crypto.h
@@ -4,12 +4,23 @@
 #include <stdint.h>
 #include <sodium.h>
 
+// Password hashing parameters
+#define IMC_OPSLIMIT 3          // Amount of operations
+#define IMC_MEMLIMIT 4096000    // Amount of memory
+
+// Error codes
+#define IMC_SUCCESS           0 // No error
+#define IMC_ERR_NO_MEMORY    -1 // No enough memory
+#define IMC_ERR_INVALID_PASS -2 // Password is not valid
+
 // Stores the secret key for encryption and the seed of the pseudorandom number generator
 typedef struct CryptoContext
 {
     uint64_t xcc20_key;
     uint64_t bbs_seed;
-};
-typedef struct CryptoContext imc_key_t;
+} CryptoContext;
+
+// Generate a secret key from a password
+int imc_crypto_context_create(char *password, CryptoContext **out);
 
 #endif  // _IMC_CRYPTO_H
