198634b5e706ad5fac65b9b46c3297ce2dfa4845
Tiago Becerra Paolini <tpaolini@gmail.com>
2023-01-25 02:45:24 -0300

    Fixed crash with JPEG reading The entire image is readed successfully now. Previously, it crashed with a SEGFAULT because the error handler was not set, and also because I was trying to read all image rows at once (now reading one at a time).

--- src/imc_image_io.c
+++ src/imc_image_io.c
@@ -9,6 +9,10 @@ int imc_jpeg_open_carrier(char *path, DataCarrier **output)
     struct jpeg_decompress_struct jpeg_obj;
     jpeg_create_decompress(&jpeg_obj);
     
+    // Use the default error handler
+    struct jpeg_error_mgr jpeg_err;
+    jpeg_obj.err = jpeg_std_error(&jpeg_err);
+    
     // Read the DCT coefficients from the image
     jpeg_stdio_src(&jpeg_obj, jpeg_file);
     jpeg_read_header(&jpeg_obj, TRUE);
@@ -22,26 +26,26 @@ int imc_jpeg_open_carrier(char *path, DataCarrier **output)
     }
 
     // Estimate the size of the array of carrier values and allocate it
-    size_t carrier_capacity = dct_count / 8;
+    size_t carrier_capacity = dct_count / 16;
     if (carrier_capacity == 0) carrier_capacity = 1;
-    JCOEFPTR *carrier_ptr = calloc(carrier_capacity, sizeof(JCOEF));
+    JCOEFPTR *carrier_ptr = calloc(carrier_capacity, sizeof(JCOEFPTR));
     size_t carrier_index = 0;
     
     // Iterate over the color components
     for (int comp = 0; comp < jpeg_obj.num_components; comp++)
     {
-        // Array of DCT coefficients for the current color component
-        JBLOCKARRAY coef_array = jpeg_obj.mem->access_virt_barray(
-            (j_common_ptr)&jpeg_obj,    // Pointer to the JPEG object
-            jpeg_dct[comp],             // DCT coefficients for the current color component
-            0,                          // The first row of DCT blocks on the image
-            jpeg_obj.comp_info[comp].height_in_blocks,  // How many rows of DCT blocks in the image
-            true                        // The array should be writeable
-        );
-
         // Iterate row by row from from top to bottom
         for (JDIMENSION y = 0; y < jpeg_obj.comp_info[comp].height_in_blocks; y++)
         {
+            // Array of DCT coefficients for the current color component
+            JBLOCKARRAY coef_array = jpeg_obj.mem->access_virt_barray(
+                (j_common_ptr)&jpeg_obj,    // Pointer to the JPEG object
+                jpeg_dct[comp],             // DCT coefficients for the current color component
+                y,                          // The current row of DCT blocks on the image
+                1,                          // Read one row of DCT blocks at a time
+                true                        // The array should be writeable
+            );
+
             // Iterate column by column from left to right
             for (JDIMENSION x = 0; x < jpeg_obj.comp_info[comp].width_in_blocks; x++)
             {
@@ -52,11 +56,11 @@ int imc_jpeg_open_carrier(char *path, DataCarrier **output)
                     if (carrier_index == carrier_capacity)
                     {
                         carrier_capacity *= 2;
-                        carrier_ptr = realloc(carrier_ptr, carrier_capacity * sizeof(JCOEF));
+                        carrier_ptr = realloc(carrier_ptr, carrier_capacity * sizeof(JCOEFPTR));
                     }
 
                     // The current coefficient
-                    const JCOEF coef = coef_array[y][x][i];
+                    const JCOEF coef = coef_array[0][x][i];
                     const JCOEFPTR coef_ptr = &coef_array[y][x][i];
 
                     // Only the AC coefficients that are greater than 1 are used as carriers
