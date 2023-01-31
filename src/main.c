9ad555ec0df5654621c6e48932de56ab9b9327ba
Tiago Becerra Paolini <tpaolini@gmail.com>
2023-01-31 18:05:13 -0300

    Template for testing

--- src/main.c
+++ src/main.c
@@ -10,7 +10,16 @@ int main(int argc, char *argv[])
     }
 
     // "C:\\Users\\tpaol\\Pictures\\17032012981.jpg"
-    imc_jpeg_open_carrier("The most awesomest image ever.jpg", NULL);
+    // imc_jpeg_open_carrier("The most awesomest image ever.jpg", NULL);
+    // imc_jpeg_open_carrier("17032012981.jpg", NULL);
+    CarrierImage *test;
+    imc_steg_init(
+        "/home/tiago/proj/imgconceal/bin/linux/debug/The most awesomest image ever.jpg",
+        (argc >= 2) ? argv[1] : "Tiago",
+        &test
+    );
+    imc_steg_insert(test, "/home/tiago/proj/imgconceal/bin/linux/debug/pintor-barroco-desconhecido-jesus-misecordioso-d.jpg");
+    imc_steg_finish(test, "teste.jpg");
     
     return 0;
 }
