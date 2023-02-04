936733e8ea22b9c26309ddb32b3aa3991b912cf5
Tiago Becerra Paolini <tpaolini@gmail.com>
2023-02-04 16:06:12 -0300

    Error for invalid "magic bytes"

--- src/globals.h
+++ src/globals.h
@@ -18,6 +18,7 @@
 #define IMC_ERR_CRYPTO_FAIL    -6   // Failed to encrypt or decrypt the data
 #define IMC_ERR_FILE_EXISTS    -7   // Output file's name already exists
 #define IMC_ERR_PAYLOAD_OOB    -8   // Out-of-bounds: attempted to read more hidden data than what is left of the image
+#define IMC_ERR_INVALID_MAGIC  -9   // The "magic bytes" of the hidden data did not match what were expected
 
 // Maximum size in bytes of the file being hidden
 #define IMC_MAX_INPUT_SIZE  500000000
