1521f7f531ece9abd1cdced3a411716ad8a3ddde
Tiago Becerra Paolini <tpaolini@gmail.com>
2023-02-15 22:04:56 -0300

    Terminal prompt for getting a password from the user

--- src/imc_cli.c
+++ src/imc_cli.c
@@ -5,7 +5,7 @@
 // Get a password from the user on the command-line. The typed characters are not displayed.
 // They are stored on the 'output' buffer, up to 'buffer_size' bytes.
 // Function returns the amount of bytes in the password.
-size_t imc_get_password(uint8_t *output, const size_t buffer_size)
+static size_t __get_password(uint8_t *output, const size_t buffer_size)
 {
     // Store the current settings of the terminal
     struct termios old_term, new_term;
@@ -32,4 +32,67 @@ size_t imc_get_password(uint8_t *output, const size_t buffer_size)
     tcsetattr(STDIN_FILENO, TCSANOW, &old_term);
 
     return pos;
+}
+
+// Allocate memory for a 'PassBuff' struct
+static PassBuff *__alloc_passbuff()
+{
+    PassBuff *pass = sodium_malloc(sizeof(PassBuff));
+    if (!pass)
+    {
+        fprintf(stderr, "Error: No enough memory\n");
+        abort();
+    }
+    sodium_memzero(pass, sizeof(PassBuff));
+    
+    pass->capacity = sizeof(pass->buffer);
+    
+    return pass;
+}
+
+// Prompt the user to input a password on the terminal.
+// The input's maximum size is determined by the macro IMC_PASSWORD_MAX_BYTES,
+// being truncated if that size is reached.
+// If 'confirm' is true, the user is asked to type the same password again.
+// Function returns NULL if the password confirmation failed.
+// The returned 'PassBuff' pointer should be freed with 'imc_cli_password_free()'.
+PassBuff *imc_cli_password_input(bool confirm)
+{
+    PassBuff *pass_1 = __alloc_passbuff();
+    
+    // Get the password for the first time
+    printf("Password: ");
+    pass_1->length = __get_password(pass_1->buffer, pass_1->capacity);
+    printf("\n");
+
+    if (confirm)
+    {
+        // Get the password for the second time if corfirmation is enabled
+        PassBuff *restrict pass_2 = __alloc_passbuff();
+        printf("Repeat password: ");
+        pass_2->length = __get_password(pass_2->buffer, pass_2->capacity);
+        printf("\n");
+
+        if (
+            pass_1->length != pass_2->length ||
+            memcmp(pass_1->buffer, pass_2->buffer, pass_1->length) != 0
+        )
+        {
+            // Fail if the two passwords do not match
+            sodium_free(pass_1);
+            sodium_free(pass_2);
+            return NULL;
+        }
+        
+        sodium_free(pass_2);
+    }
+
+    return pass_1;
+}
+
+// Free the memory of a 'PassBuff' struct
+void imc_cli_password_free(PassBuff *password)
+{
+    sodium_free(password);
+    // Note: the above function already overwrites the memory before freeing it.
 }
--- src/imc_cli.h
+++ src/imc_cli.h
@@ -5,9 +5,32 @@
 
 #include "imc_includes.h"
 
+#define IMC_PASSWORD_MAX_BYTES 4080     // Size (in bytes) of the password buffer
+
+// Buffer for the plaintext password
+typedef struct PassBuff {
+    size_t capacity;    // The maximum amount of bytes that the buffer can store
+    size_t length;      // The current amount of bytes stored on the buffer
+    uint8_t buffer[IMC_PASSWORD_MAX_BYTES]; // Array of bytes with the plaintext password
+} PassBuff;
+
 // Get a password from the user on the command-line. The typed characters are not displayed.
 // They are stored on the 'output' buffer, up to 'buffer_size' bytes.
 // Function returns the amount of bytes in the password.
-size_t imc_get_password(uint8_t *output, const size_t buffer_size);
+static size_t __get_password(uint8_t *output, const size_t buffer_size);
+
+// Allocate memory for a 'PassBuff' struct
+static PassBuff *__alloc_passbuff();
+
+// Prompt the user to input a password on the terminal.
+// The input's maximum size is determined by the macro IMC_PASSWORD_MAX_BYTES,
+// being truncated if that size is reached.
+// If 'confirm' is true, the user is asked to type the same password again.
+// Function returns NULL if the password confirmation failed.
+// The returned 'PassBuff' pointer should be freed with 'imc_cli_password_free()'.
+PassBuff *imc_cli_password_input(bool confirm);
+
+// Free the memory of a 'PassBuff' struct
+void imc_cli_password_free(PassBuff *password);
 
 #endif  // _IMC_CLI_H
