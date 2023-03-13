30d161570a05e68b7ace6dac8c8245918918baaa
Tiago Becerra Paolini <tpaolini@gmail.com>
2023-03-13 13:13:05 -0300

    Checking if the `--hide` option has been specified

--- src/imc_cli.c
+++ src/imc_cli.c
@@ -196,15 +196,15 @@ static inline void __execute_options(struct argp_state *state, void *options)
     UserOptions *opt = (UserOptions*)options;
 
     // Check if the user has specified exactly one operation
-    int mode_count = (bool)opt->input + (bool)opt->extract + (bool)opt->check;
+    int mode_count = (bool)opt->hide.data + (bool)opt->extract + (bool)opt->check;
 
     if (mode_count == 0)
     {
-        argp_error(state, "you must specify either the 'input', 'extract', or 'check' option.");
+        argp_error(state, "you must specify either the 'hide', 'extract', or 'check' option.");
     }
-    else if (mode_count > 1)
+    else if (mode_count != 1)
     {
-        argp_error(state, "you can only specify one of the 'input', 'extract', or 'check' options.");
+        argp_error(state, "you can specify only one among the 'hide', 'extract', or 'check' options.");
     }
 
     // Display a password prompt, if a password wasn't provided
