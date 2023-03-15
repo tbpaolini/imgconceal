4a4a7d8f459aa7d500f70452afcbdd1adb55f417
Tiago Becerra Paolini <tpaolini@gmail.com>
2023-03-15 18:31:51 -0300

    Storing the `--verbose` and `--check` flags in the image struct

--- src/imc_cli.c
+++ src/imc_cli.c
@@ -311,6 +311,10 @@ static inline void __execute_options(struct argp_state *state, void *options)
             break;
     }
 
+    // Store the '--verbose' and '--check' flags
+    steg_image->just_check = opt->check;
+    steg_image->verbose = opt->verbose;
+
     // Operation on the image
     if (mode == HIDE)
     {
