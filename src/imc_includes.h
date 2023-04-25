e7605a9da0c31703c4e87e4c5b62cb4aad96c504
Tiago Becerra Paolini <tpaolini@gmail.com>
2023-04-25 13:06:13 -0300

    Included the headers needed by the Windows's build of imgconceal

--- src/imc_includes.h
+++ src/imc_includes.h
@@ -3,6 +3,12 @@
 #ifndef _IMC_INCLUDES_H
 #define _IMC_INCLUDES_H
 
+#ifdef _WIN32
+#include <winsock2.h>   // Needed by 'endian.h'
+/* Note: this is included before anything else because otherwise there would be
+some conflicts with the symbols defined by `windows.h` or the standard libraries */
+#endif // _WIN32
+
 // Standard libraries
 #include <stdlib.h>
 #include <stdio.h>
@@ -18,14 +24,16 @@
 // System libraries
 #ifdef _WIN32
 #include <windows.h>    // Microsoft Windows API
-#endif // _WIN32
+#include <io.h>         // For the _get_osfhandle() functions
+#else // Linux / Unix
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <libgen.h>     // For the basename() function
-#include <endian.h>
 #include <fcntl.h>      // For the AT_FDCWD macro
 #include <termios.h>    // For temporarily turning off input echoing in the terminal
+#endif // _WIN32
+#include <endian.h>     // Converting between different byte orders
 #include <argp.h>       // Command line interface
 
 // Third party libraries
