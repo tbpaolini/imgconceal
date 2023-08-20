e970060ac622e9f706d251444e4b9896544f6000
Tiago Becerra Paolini <tpaolini@gmail.com>
2023-08-20 11:30:39 -0300

    Tweaked comment

--- src/imc_includes.h
+++ src/imc_includes.h
@@ -25,7 +25,7 @@ some conflicts with the symbols defined by `windows.h` or the standard libraries
 // System libraries
 #ifdef _WIN32
 #include <windows.h>    // Microsoft Windows API
-#include <io.h>         // For the _get_osfhandle() function
+#include <io.h>         // For the _get_osfhandle() and _access() functions
 #include <direct.h>     // _getcwd(), _mkdir(), _chdir(), _rmdir()
 #else // Linux / Unix
 #include <unistd.h>
